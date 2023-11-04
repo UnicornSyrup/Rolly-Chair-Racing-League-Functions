eeacf443a83d3e3cae164881a9b1e670b0b689e8
Jasper Holland <83934607+UnicornSyrup@users.noreply.github.com>
2023-11-04 18:42:42 -0500

    Update httpTrigger1.js

--- src/functions/httpTrigger1.js
+++ src/functions/httpTrigger1.js
@@ -1,10 +1,11 @@
 const { app } = require('@azure/functions');
-const { MongoClient } = require("mongodb");
 app.http('httpTrigger1', {
     methods: ['GET',],
     authLevel: 'anonymous',
     handler: async (request, context) => {
+        const { MongoClient } = require("mongodb");
         
+
         let uri = `mongodb+srv://dbUser:${process.env.MONGO_PASSWORD}@cluster0.isxlqf2.mongodb.net/?retryWrites=true&w=majority`;
         // Create a MongoClient with a MongoClientOptions object to set the Stable API version
         let client = new MongoClient(uri, {

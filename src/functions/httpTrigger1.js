4a698c42fe1e784b9ee501aa63b5272f033d0a23
Jasper Holland <83934607+UnicornSyrup@users.noreply.github.com>
2023-11-04 16:42:25 -0500

    Update httpTrigger1.js

--- src/functions/httpTrigger1.js
+++ src/functions/httpTrigger1.js
@@ -1,6 +1,6 @@
+/*
 const { app } = require('@azure/functions');
 const { MongoClient } = require("mongodb");
-const mongoClient = new MongoClient(process.env.MONGO_URI);
 app.http('httpTrigger1', {
     methods: ['GET',],
     authLevel: 'anonymous',
@@ -17,3 +17,18 @@ app.http('httpTrigger1', {
         return { body: `Hello, ${name}! ${results.length}` };
     }
 });
+*/
+
+const { MongoClient } = require("mongodb");
+module.exports = async function(context) {
+        const database = await mongoClient.db("cluster0")
+        const collection = database.collection("races");
+
+        const results = await collection.find({}).limit(10).toArray();
+
+        context.log(`Http function processed request for url "${request.url}"`);
+
+        const name = request.query.get('name') || await request.text() || 'world';
+
+        return { body: `Hello, ${name}! ${results.length}` };
+    };

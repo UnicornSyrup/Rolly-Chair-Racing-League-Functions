f96f85130cfb77034344ece51c1f690e614599b7
Jasper Holland <83934607+UnicornSyrup@users.noreply.github.com>
2023-11-04 18:46:16 -0500

    Update httpTrigger1.js

--- src/functions/httpTrigger1.js
+++ src/functions/httpTrigger1.js
@@ -5,9 +5,10 @@ app.http('httpTrigger1', {
     handler: async (request, context) => {
         const { MongoClient } = require("mongodb");
         
-
         let uri = `mongodb+srv://dbUser:${process.env.MONGO_PASSWORD}@cluster0.isxlqf2.mongodb.net/?retryWrites=true&w=majority`;
         // Create a MongoClient with a MongoClientOptions object to set the Stable API version
+
+        /*
         let client = new MongoClient(uri, {
         serverApi: {
             version: ServerApiVersion.v1,
@@ -31,6 +32,8 @@ app.http('httpTrigger1', {
             // Ensures that the client will close when you finish/error
             await client.close();
         }
+
+        */
         
         
 

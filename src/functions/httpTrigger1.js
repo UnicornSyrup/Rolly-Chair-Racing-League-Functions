587d1fac02d80d0621f3fed9414b496104821b30
Jasper Holland <83934607+UnicornSyrup@users.noreply.github.com>
2023-11-04 20:09:45 -0500

    add internal require statement

--- src/functions/httpTrigger1.js
+++ src/functions/httpTrigger1.js
@@ -4,6 +4,8 @@ app.http('httpTrigger1', {
     methods: ['GET',],
     authLevel: 'anonymous',
     handler: async (request, context) => {
+        const client = require('mongodb');
+
         context.log(`Http function processed request for url "${request.url}"`);
 
         const name = request.query.get('name') || await request.text() || 'world';

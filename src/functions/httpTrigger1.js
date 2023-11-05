d2d0e3a34779f3f6e2dcf5bc3c3264db49029642
Jasper Holland <83934607+UnicornSyrup@users.noreply.github.com>
2023-11-04 20:05:05 -0500

    revert to last working state

--- src/functions/httpTrigger1.js
+++ src/functions/httpTrigger1.js
@@ -4,44 +4,10 @@ app.http('httpTrigger1', {
     methods: ['GET',],
     authLevel: 'anonymous',
     handler: async (request, context) => {
-        // const { MongoClient } = require("mongodb");
-        
-        // let uri = `mongodb+srv://dbUser:${process.env.MONGO_PASSWORD}@cluster0.isxlqf2.mongodb.net/?retryWrites=true&w=majority`;
-        // Create a MongoClient with a MongoClientOptions object to set the Stable API version
-
-        /*
-        let client = new MongoClient(uri, {
-        serverApi: {
-            version: ServerApiVersion.v1,
-            strict: true,
-            deprecationErrors: true,
-        }
-        });
-
-        try {
-            // Connect the client to the server	(optional starting in v4.7)
-            await client.connect();
-            // Send a ping to confirm a successful connection
-            // await client.db("admin").command({ ping: 1 });
-            // console.log("Pinged your deployment. You successfully connected to MongoDB!");
-
-            const database = await client.db("cluster0")
-            const collection = await database.collection("races");
-
-            const results = await collection.find({}).limit(10).toArray();
-        } finally {
-            // Ensures that the client will close when you finish/error
-            await client.close();
-        }
-
-        */
-        
-        
-
         context.log(`Http function processed request for url "${request.url}"`);
 
         const name = request.query.get('name') || await request.text() || 'world';
 
-        return { body: `Hello, ${name}! ${results.length}` };
+        return { body: `Hello, ${name}!` };
     }
 });
